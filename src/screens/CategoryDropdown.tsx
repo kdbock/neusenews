a9baf9b15cd345dafaf136e2b0692416fac472b9
Kristy Kelly <kdbock79@icloud.com>
2025-03-06 06:21:34 -0500

    Add files via upload

---
+++ src/screens/CategoryDropdown.tsx
@@ -0,0 +1,11 @@
+// app/components/CategoryDropdown.tsx
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function CategoryDropdown() {
+  return (
+    <View>
+      <Text>Category Dropdown</Text>
+    </View>
+  );
+}
